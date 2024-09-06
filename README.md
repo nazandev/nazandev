53b701da011ffc0a82d1a42a353c184f4098fe3c
Jonathan <nazandeveloper@gmail.com>
2024-09-07 01:37:39 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+  <a href="" target="_blank"><img src=""></a>
+</p>
+<h3 align="center"> Hi! 👋 I'm nazandev 👨🏻‍💻</h3>
+Sobre mi
+
+### 💼 [Technical Skills](#-technical-skills)
+![PHP](https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white)
+![MYSQL](https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white)
+![JS](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![NODEJS](https://img.shields.io/badge/Node%20js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+### 💻 [Youtube Channel](https://youtube.com/@nazandev?sub_confirmation=1)
+<!-- YOUTUBE:START -->
+<a href='https://www.youtube.com/watch?v=M7F6tOaEOcA' target='_blank'>
+              <img width='30%' src='https://i.ytimg.com/vi/M7F6tOaEOcA/hqdefault.jpg'/>
+            </a>
+<!-- YOUTUBE:END -->
+
+### 📈 [GitHub Stats](#-github-stats)
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=nazandev&show_icons=true&theme=tokyonight)
+
+### ⌨️ [Languages](#%EF%B8%8F-languages)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=nazandev&layout=compact&theme=tokyonight)
---
+++ scripts/updateReadmeWithYoutubeLatestVideos.js
@@ -0,0 +1,86 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+const path = require('path');
+
+const API_KEY = process.env.YOUTUBE_API_KEY;
+const CHANNEL_ID = 'UCeyKkt1JrO90CjV5WCsS-xA';
+const README_PATH = '../README.md';
+const MAX_RESULTS = 3
+const START_MARKER = '<!-- YOUTUBE:START -->';
+const END_MARKER = '<!-- YOUTUBE:END -->';
+const ENCODING = 'utf-8';
+
+async function getLatestVideos() {
+  console.log('Getting Youtube latest videos');
+  const youtube = google.youtube({
+    version: 'v3',
+    auth: API_KEY,
+  });
+
+  const response = await youtube.search.list({
+    part: 'snippet',
+    channelId: CHANNEL_ID,
+    maxResults: MAX_RESULTS,
+    order: 'date',
+    type: 'video',
+    videoDuration: 'any' // long medium short
+  });
+
+  const videos = response.data.items.map(item => {
+    const videoId = item.id.videoId;
+    const title = item.snippet.title;
+    const thumbnailUrl = item.snippet.thumbnails.high.url;
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    // return `### [${title}](${videoUrl})\n[![${title}](${thumbnailUrl})](${videoUrl})`;
+    return `<a href='${videoUrl}' target='_blank'>
+              <img width='30%' src='${thumbnailUrl}'/>
+            </a>`;
+  });
+
+  return videos.join('\n');
+}
+
+function getReadmeContent() {
+  console.log('Getting README content');
+  const readmePath = path.join(__dirname, README_PATH);
+  const readmeContent = fs.readFileSync(readmePath, { encoding: ENCODING });
+  return readmeContent;
+}
+
+function updateReadme(videos) {
+  console.log('Updating README');
+
+  const readmeContent = getReadmeContent();
+  const startIndex = readmeContent.indexOf(START_MARKER);
+  const endIndex = readmeContent.indexOf(END_MARKER);
+
+  const hasStartMarker = startIndex !== -1;
+  const hasEndMarker = endIndex !== -1;
+  const areMarkersWellPositioned =
+    hasStartMarker &&
+    hasEndMarker &&
+    ((startIndex + START_MARKER.length) < endIndex);
+
+  if (!hasStartMarker || !hasEndMarker || !areMarkersWellPositioned) {
+    throw new Error('README markers not found or are misconfigured.');
+  }
+
+  const updatedReadmeContent = readmeContent.substring(0, startIndex + START_MARKER.length) + '\n' + videos + '\n' + readmeContent.substring(endIndex);
+
+  fs.writeFileSync(README_PATH, updatedReadmeContent);
+  console.log('README updated with the latest YouTube videos');
+}
+
+async function start() {
+  console.log('Script started');
+  try {
+    const videos = await getLatestVideos();
+    updateReadme(videos);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log('Script finished');
+  }
+}
+
+start();
